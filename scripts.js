0c44faa7ded9eaf4ccbddef567b8d6d3a3e9d0c1
unknown <Eusousleandro>
2026-07-22 15:40:47 -0300

    implementaçao da estrutura do project e contact do portfolio

--- scripts.js
+++ scripts.js
@@ -1,12 +1,26 @@
+const Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+
+    console.log(entries)
+})
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach((element) => Observer.observe(element))
+
 function EnviarWhats(event) {
     event.preventDefault()
-    
-    const name = document.getElementById(#name)
-    const message = document.getElementById(#message)
+    const name = document.getElementById('#name')
+    const message = document.getElementById('#message')
     const phone = "83921470522"
-
     const url = `Olá ${name}, ${message}`
 
     window.open(url, '_black')
-
 }
+
+alert('Hello')
