b9cb75774b4b43d4bb282600a2748556c55e6b15
unknown <Eusousleandro>
2026-07-28 04:13:49 -0300

    Refaturando o código

--- js/data/projects.js
+++ js/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
         "title": "Clinic Management",
         "description": "Em desenvolvimento, Sistema de gerenciamento de clínicas desenvolvido com FastAPI, MySQL e SQLAlchemy, O projeto contará com auteticação JWT, gerenciamento de pacientes, médicos, consultas e arquitetura organizada seguindo boas práticas de desenvolvimento.",
         "demo": "#",
-        "github": "https://github/Eusousleandro"
+        "github": "https://github.com/Eusousleandro/Clinic_management"
     },
 
     {
@@ -12,7 +12,7 @@ export const projects = [
         "title": "Clinic Management",
         "description": "Em desenvolvimento, Sistema de gerenciamento de clínicas desenvolvido com FastAPI, MySQL e SQLAlchemy, O projeto contará com auteticação JWT, gerenciamento de pacientes, médicos, consultas e arquitetura organizada seguindo boas práticas de desenvolvimento.",
         "demo": "#",
-        "github": "https://github/Eusousleandro"
+        "github": "https://github.com/Eusousleandro/Clinic_management"
     },
 
     {
