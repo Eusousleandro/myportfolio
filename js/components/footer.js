d4381ba9c4b0ab2fa249c367cbbb5dafa135a868
unknown <Eusousleandro>
2026-07-30 12:10:45 -0300

    Implementando os buttons do hero e atualizando os estilos do navbar.

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,5 +1,9 @@
 export default function Footer() {
     return `
-        <h2>Footer</h2>
+        <footer class="cta-footer">
+                <h2 class="text-footer">© 2026Leandro Sousa Silva.</h2>text-footer
+                <span class="sub-text-footer">Todos os direitos reservados.</span>
+            </div>
+        </footer>
     `
 }
--- js/pages/home.js
+++ js/pages/home.js
@@ -11,6 +11,25 @@ export default function Home() {
                 </p>
             </div>
             <img src="" alt="Foto do perfil" class="img-profile">
+            <div class="hero-buttons">
+                <a href="#projects" class="btn-primary" target="_blank">
+                    Ver projetos
+                </a>
+                <a href="" class="btn-secondary" download>
+                    Download
+                </a>
+            </div>
+            <div class="hero-social">
+                <a href="">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+                <a href="">
+                    <i class="fa-brands fa-linkedin"></i>
+                </a>
+                <a href="">
+                    <i class="fa-solid fa-envelope"></i>
+                </a>
+            </div>
         </section>
     `
 }
