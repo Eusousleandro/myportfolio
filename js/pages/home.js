20973018db05d068b335a3edae9777d05ea84037
unknown <Eusousleandro>
2026-07-30 02:10:23 -0300

    Refaturando e organizando o código

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,6 @@
 export default function Home() {
     return `
         <section id="profile" class="profile hidden">
-            <img src="" alt="Foto do perfil" class="img-profile">
             <div class="text-profile">
                 <h1>Olá, </h1>
                 <h2 class="text-name">Eu sou o Leandro Sousa</h2>
@@ -11,6 +10,7 @@ export default function Home() {
                     FastApi, MySQL, Docker e APIs.
                 </p>
             </div>
+            <img src="" alt="Foto do perfil" class="img-profile">
         </section>
     `
 }
