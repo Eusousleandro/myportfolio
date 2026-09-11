a1c178cb345b09143fafd281e5c809bf64ceeb39
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-09-11 14:19:28 -0300

    Implementando os links dos peojetos

--- js/data/projects.js
+++ js/data/projects.js
@@ -2,9 +2,9 @@ export const projects = [
     {
         "image": "./assets/images/OIP.webp",
         "title": "Delivery APis",
-        "description": "Sistema de gerenciamento de clínicas desenvolvido com FastAPI, MySQL e SQLAlchemy, O projeto contará com auteticação JWT, gerenciamento de pacientes, médicos, consultas e arquitetura organizada seguindo boas práticas de desenvolvimento.",
+        "description": "API de delivery de comida com gerenciamento de pedidos, usuários e restaurantes, incluindo autenticação e atualização de status em tempo real.",
         "demo": "#",
-        "github": "https://github.com/Eusousleandro/Clinic_management"
+        "github": "https://github.com/Eusousleandro/delivery_system_api"
     },
 
     {
@@ -18,8 +18,8 @@ export const projects = [
     {
         "image": "./assets/images/OIP.webp",
         "title": "Attendence List APis",
-        "description": "Em desenvolvimento, Sistema de gerenciamento de clínicas desenvolvido com FastAPI, MySQL e SQLAlchemy, O projeto contará com auteticação JWT, gerenciamento de pacientes, médicos, consultas e arquitetura organizada seguindo boas práticas de desenvolvimento.",
+        "description": "API backend para gerenciamento de listas de presença, construída com Node.js, TypeScript, Express e MySQL.",
         "demo": "#",
-        "github": "https://github/Eusousleandro"
+        "github": "https://github.com/Eusousleandro/attendance-list-server"
     }    
 ]
